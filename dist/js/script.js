a62491f4952ea31627f1e1131a71f0c5e43d9edf
Megha327 <megha@tothenew.com>
2020-05-08 02:28:22 +0530

    commit code with fully responsive website

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,3 +12,13 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
