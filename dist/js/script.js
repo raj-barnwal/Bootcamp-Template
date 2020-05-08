0d70c74257c78e85c36eeeb50084726713fb64a3
scoopspoofsv <shivam.pandey@tothenew.com>
2020-05-08 20:45:17 +0530

    - Added slider gallery for clients section - Added map in contact section

--- dist/js/script.js
+++ dist/js/script.js
@@ -35,3 +35,17 @@ function validateForm() {
         return false;
     }
 }
+
+// FOR MAP
+
+function init() {
+    map = new OpenLayers.Map("basicMap");
+    var mapnik = new OpenLayers.Layer.OSM();
+    var fromProjection = new OpenLayers.Projection("EPSG:4326"); // Transform from WGS 1984
+    var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+    var position = new OpenLayers.LonLat(13.41, 52.52).transform(fromProjection, toProjection);
+    var zoom = 15;
+
+    map.addLayer(mapnik);
+    map.setCenter(position, zoom);
+}
