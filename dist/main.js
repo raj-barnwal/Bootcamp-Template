9621efaf532d9b61d79865766b045eef73428394
Ananay10 <60641598+Ananay10@users.noreply.github.com>
2020-05-08 20:28:50 +0530

    Code Uploaded with animation

---
+++ dist/main.js
@@ -0,0 +1,25 @@
+function initMap() {
+  // Your location
+  const loc = { lat: 28.613939, lng: 77.209023 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc,
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Smooth Scrolling
+// $(".btn").on("click", function (event) {
+//   if (this.hash !== "") {
+//     event.preventDefault();
+//     const hash = this.hash;
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(hash).offset().top,
+//       },
+//       800
+//     );
+//   }
+// });
